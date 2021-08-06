991cd4385140bc25d08dfde6b8a71a26be45b31d
Riga <rigovaalina@ya.ru>
2021-08-06 21:19:32 +0300

    button added

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-
+const button = document.querySelector('button')
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 let mouseX,
 mouseY,
 radius = 40,
 center = 400,
-    circleX = 400,
-    circleY = 400,
-    border = 255
+circleX = 400,
+circleY = 400,
+border = 255,
+bonusFlag = false
 
 window.addEventListener('mousemove', (event) => {
     mouseY = event.clientY
     mouseX = event.clientX
     drawCircle()
+})
+
+button.addEventListener('click', () =>{
+    if (!bonusFlag)
+        bonusFlag = true
+    else
+        bonusFlag = false
 })
 
 function newPow(circleX, circleY){
@@ -21,6 +29,17 @@ function newPow(circleX, circleY){
 }
 
 function checkCoords() {
+    let factor = border / newPow(mouseX, mouseY)
+    if (newPow(mouseX, mouseY) < border) {
+        circleX = mouseX;
+        circleY = mouseY;
+    } else {
+        circleX = center + (mouseX - center) * factor
+        circleY = center + (mouseY - center) * factor
+    }
+}
+
+function checkCoordsBonus() {
 if (mouseY - circleY < 100 && mouseX - circleX < 100) {
     if (mouseX - circleX > 0 && newPow(circleX - 5, circleY) < border)
         circleX -= 5;
@@ -34,7 +53,10 @@ if (mouseY - circleY < 100 && mouseX - circleX < 100) {
 }
 
 function drawCircle () {
-    checkCoords()
+    if (!bonusFlag)
+        checkCoordsBonus()
+    else
+        checkCoords()
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.beginPath()
     ctx.arc(circleX , circleY , radius, 0, Math.PI * 2, true)
